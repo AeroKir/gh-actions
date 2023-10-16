0882d44144731a30dc6d3fa0643589ebb1725fad
Kirill Shevtsov <shev.kv.1982@gmail.com>
2023-10-16 23:27:00 +0300

    test gh action

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>Add test CI/CD!!!</p>
+      <p>Add test CI/CD</p>
       <p>Push to the server!!!</p>
       <button>Push</button>
     </>
